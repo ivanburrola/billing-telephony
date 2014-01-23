e6fe9cf769a66dd0b36b8fdce3c0687894d7a4e6
Gabriel Medina <gmm@transtelco.net>
2014-01-23 02:53:13 -0700

    Final NetSuite fetcher

--- netsuite_customer_billing_info_fetch_test.rb
+++ netsuite_customer_billing_info_fetch_test.rb
@@ -2,12 +2,14 @@
 
 # encoding: utf-8
 
+require 'yaml'
+
 require "./lib/net_suite"
 
 res = NetSuite.call(action: "customer", customer_id: 14487)
 
 if res.status == :ok
-	pp res.data
+	puts res.data.to_yaml
 else
 	puts "Error: "
 	pp res
